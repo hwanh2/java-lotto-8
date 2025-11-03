5db6b6e95088d600d006e723155ad12f77d564db
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-11-03 12:55:18 +0900

    refactor: 중복 정렬 제거(생성 클래스는 생성만 하도록 변경)

--- src/main/java/lotto/generator/RandomLottoGenerator.java
+++ src/main/java/lotto/generator/RandomLottoGenerator.java
@@ -9,7 +9,6 @@ public class RandomLottoGenerator {
     public Lotto generate(){
         List<Integer> lotto = Randoms.pickUniqueNumbersInRange(Lotto.MIN_LOTTO_NUM, Lotto.MAX_LOTTO_NUM, Lotto.LOTTO_NUMBER_COUNT)
                 .stream()
-                .sorted()
                 .toList();
         return new Lotto(lotto);
     }
